e392a87f5b184bb527adf4851e0a651187a1b34b
ReddishBlue <toristarynski@gmail.com>
2023-09-16 18:52:12 -0400

    attempt at sending text message

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request
 from pyngrok import ngrok
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.voice_response import VoiceResponse
 
 happiness = 25
 max_happiness = 50
 med_names = []
 med_times = []
+phone_number = 0000000000
+
+#stuff to send text messages
+# Your Account SID from twilio.com/console
+account_sid = "AC1301b04dcb50efa82ad50164c3c4eb9e"
+# Your Auth Token from twilio.com/console
+auth_token  = "998b3ec8a9b64fbaedeca6f4aaeef2b4"
+client = Client(account_sid, auth_token)
+
 
 app = Flask(__name__)
 
@@ -19,9 +31,18 @@ def enter_medication():
     if request.method == 'POST':
         med_name = request.form.get("submit_med")
         med_time = request.form.get("submit_time")
+        phone_number = request.form.get("phone_number")
         
         med_names.append(med_name)
         med_times.append(med_time)
+        
+        message = client.messages.create(
+            to="{}{}".format("+", phone_number), 
+            from_="+18445610671",
+            body="You have set up your text notifications!")
+        
+       
+        print(message.sid)
         return render_template('Component3.html')
     else:
         # Handle the GET request (or other methods) here
@@ -36,6 +57,8 @@ def main_page():
 @app.route('/Insert')
 def insert():
     return render_template('Compnent.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
