b726ace93845643b68ec6d98226c293ee2a06f90
ReddishBlue <toristarynski@gmail.com>
2023-09-16 11:37:28 -0400

    edit medication page outline

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+@app.route('/EnterMedication')
+
+def enter_medication():
+    return render_template('Component3.html')
+
+@app.route('/MainPage')
+
+def main_page():
+    return render_template('Component5.html')
+
 if __name__ == '__main__':
     app.run(debug = True)
