aba17b90069c75df1df305c3f682162aa15f2c17
ronaldngounou <landryzinkoue@yahoo.fr>
2023-09-16 12:24:50 -0400

    worked on t

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+
+@app.route('/editMedication')
+
+def edit_medication():
+    return render_template('Component5.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug = True)
