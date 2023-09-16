51783af4ba718f10376815a15b5eb467909e0688
ReddishBlue <toristarynski@gmail.com>
2023-09-16 13:24:44 -0400

    put medication name and time into arrays

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 happiness = 25
 max_happiness = 50
+med_names = []
+med_times = []
 
 app = Flask(__name__)
 
@@ -10,9 +12,22 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
-@app.route('/EnterMedication')
+@app.route('/EnterMedication', methods=['POST', 'GET'])
 
 def enter_medication():
+    if request.method == 'POST':
+        med_name = request.form.get("submit_med")
+        med_time = request.form.get("submit_time")
+        
+        med_names.append(med_name)
+        med_times.append(med_time)
+        return render_template('Component3.html')
+    else:
+        # Handle the GET request (or other methods) here
+        return render_template('Component3.html')
+    
+    print(f"med: {med_name}")
+    print(f"time: {med_time}")
     return render_template('Component3.html')
 
 @app.route('/MainPage')
